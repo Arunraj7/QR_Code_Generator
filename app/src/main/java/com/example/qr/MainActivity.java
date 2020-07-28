70186d4474eb8fafa448e86261ac190d541b8501
Arun <arun@localhost.localdomain>
2020-07-28 19:21:37 +0530

    QR

--- app/src/main/java/com/example/qr/MainActivity.java
+++ app/src/main/java/com/example/qr/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,13 +15,47 @@ import com.google.zxing.WriterException;
 
 import java.util.Scanner;
 
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Random;
+
+
 import androidmads.library.qrgenearator.QRGContents;
 import androidmads.library.qrgenearator.QRGEncoder;
 
 public class MainActivity extends AppCompatActivity {
     EditText qrvalue;
-    Button generateBtn, scanBtn;
+    Button generateBtn, scanBtn, downbtn;
     ImageView qrImage;
+    Bitmap qrBits;
+
+
 
 
     @Override
@@ -32,16 +67,22 @@ public class MainActivity extends AppCompatActivity {
         generateBtn = findViewById(R.id.button);
         scanBtn = findViewById(R.id.button2);
         qrImage = findViewById(R.id.imageView);
+        downbtn = findViewById(R.id.button3);
+        downbtn.setVisibility(View.GONE);
+
+
 
         generateBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String data = qrvalue.getText().toString();
-                if(data.isEmpty()){
+                if (data.isEmpty()) {
                     qrvalue.setError("Value Required");
-                }else {
+                } else {
+                    downbtn.setVisibility(View.VISIBLE);
+
                     QRGEncoder qrgEncoder = new QRGEncoder(data, null, QRGContents.Type.TEXT, 700);
-                    Bitmap qrBits = qrgEncoder.getBitmap();
+                    qrBits = qrgEncoder.getBitmap();
                     qrImage.setImageBitmap((qrBits));
                 }
             }
@@ -52,5 +93,8 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(), scanner.class));
             }
         });
+
+       
     }
+
 }
