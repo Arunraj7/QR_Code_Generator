6ce85722248278349c18bb888cb937535ce4b5ae
Arun <arun@localhost.localdomain>
2020-07-28 20:47:49 +0530

    QR

--- app/src/main/java/com/example/qr/MainActivity.java
+++ app/src/main/java/com/example/qr/MainActivity.java
@@ -3,47 +3,25 @@ package com.example.qr;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
-import com.google.zxing.WriterException;
 
-import java.util.Scanner;
-
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-
-import android.Manifest;
-import android.app.Activity;
-import android.content.Context;
-import android.content.ContextWrapper;
-import android.content.Intent;
-import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
-import android.media.MediaScannerConnection;
-import android.net.Uri;
-import android.os.Bundle;
+
 import android.os.Environment;
-import android.provider.MediaStore;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.Toast;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.OutputStream;
-import java.util.Random;
 
 
 import androidmads.library.qrgenearator.QRGContents;
@@ -54,6 +32,8 @@ public class MainActivity extends AppCompatActivity {
     Button generateBtn, scanBtn, downbtn;
     ImageView qrImage;
     Bitmap qrBits;
+    Bitmap bitmap;
+    private static  final int WRITE_EXTERNAL_STORAGE_CODE = 1;
 
 
 
@@ -69,6 +49,13 @@ public class MainActivity extends AppCompatActivity {
         qrImage = findViewById(R.id.imageView);
         downbtn = findViewById(R.id.button3);
         downbtn.setVisibility(View.GONE);
+
+//        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+//            if(checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED){
+//                String[] permission = (Manifest.permission.WRITE_EXTERNAL_STORAGE);
+//                requestPermissions(permission, WRITE_EXTERNAL_STORAGE_CODE);
+//            }
+//        }
 
 
 
@@ -94,7 +81,33 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-       
+        downbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                bitmap = ((BitmapDrawable)qrImage.getDrawable()).getBitmap();
+                String time = String.valueOf(qrvalue.getText());
+                File path = Environment.getExternalStorageDirectory();
+                File dir = new File(path+"/DCIM");
+                dir.mkdirs();
+                String imagename = time+".png";
+                File file = new File(dir, imagename);
+                OutputStream out;
+
+                try {
+                    out = new FileOutputStream(file);
+                    bitmap.compress(Bitmap.CompressFormat.PNG,100,out);
+                    out.flush();
+                    out.close();
+                    Toast.makeText(MainActivity.this, "Image Saved", Toast.LENGTH_SHORT).show();
+                } catch (Exception e){
+                    Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
     }
+
+
 
 }
